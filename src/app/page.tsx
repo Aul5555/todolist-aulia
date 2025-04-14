e96169a33dae2b587b472c4ad95f016b46f18936
aul5555 <auliarahman5790@gmail.com>
2025-04-14 19:49:47 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from '../components/todolist'; 
+
+function Page() {
+  return (
+    <>
+      <h1></h1>
+      <TodoList /> {}
+    </>
+  );
+}
+
+export default Page;
